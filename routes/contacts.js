5e0fac9bbfb912a5a6e9da94e2d56b3bae726810
smith-tanner22 <smith.tanner2222@gmail.com>
2022-04-29 17:36:41 -0600

    complete personal project

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,6 +5,6 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 
-router.get('/', contactsController.getSingle);
+router.get('/:id', contactsController.getSingle);
 
 module.exports = router;
