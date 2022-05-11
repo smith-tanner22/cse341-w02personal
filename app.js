389900210bb83d53467b66e2ec3866c95abd6d00
smith-tanner22 <smith.tanner2222@gmail.com>
2022-05-11 08:37:56 -0600

    finished personal project04

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+var options = {
+  explorer: true
+};
+
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
+  .use(cors())
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
