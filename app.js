6e398a4860cf94661fbb99b1270107619f7ec09d
smith-tanner22 <smith.tanner2222@gmail.com>
2022-05-12 13:12:48 -0600

    added functionality to react form

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
+
   .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
